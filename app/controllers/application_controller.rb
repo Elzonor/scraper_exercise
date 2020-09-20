10598797d2d45a49e7573526bf6c416bc845a9db
Lorenzo Ciglioni <lorenzo.ciglioni@gmail.com>
2020-09-20 15:20:25 +0000

    Created first version of the scraper

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+class ApplicationController < ActionController::Base
+
+	# Define the Entry object
+	class Entry
+		def initialize(title, link)
+			@title = title
+			@link  = link
+		end
+		attr_reader :title
+		attr_reader :link
+	end
+	
+	def scrape_site
+		# Pull in the page
+		require 'open-uri'
+		doc = Nokogiri::HTML(open("https://www.repubblica.it/"))
+		
+		# Narrow down on what we want and build the entries array
+		@entries = doc.css('.entry-title')
+		@entriesArray = []
+		@entries.each do |entry|
+			title = entry.css('a').text
+			link = entry.css('a')[0]['href']
+			@entriesArray << Entry.new(title, link)
+		end
+	end
+	
+end
